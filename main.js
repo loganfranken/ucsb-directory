8bc62e60a4ad5a772ed3bc9f138b04929007f826
Logan Franken <loganfranken@gmail.com>
2015-11-19 16:17:29 -0800

    Added basic request

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+var request = require('request');
+
+var url = 'http://www.identity.ucsb.edu/people_finder/?rs=search_person_records&rsargs=%5B%22person%22%2C%22logan%20franken%22%5D&';
+
+request(url, function (error, response, body) {
+
+  console.log(body);
+
+});
